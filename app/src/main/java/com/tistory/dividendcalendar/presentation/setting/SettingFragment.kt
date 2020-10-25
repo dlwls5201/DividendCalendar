9296d072b32906df7a61693bdaa3fb778e8fb55e
dlwls5201 <dlwls5201@naver.com>
2020-10-25 21:20:36 +0900

    설정 탭 작업

--- app/src/main/java/com/tistory/dividendcalendar/presentation/setting/SettingFragment.kt
+++ app/src/main/java/com/tistory/dividendcalendar/presentation/setting/SettingFragment.kt
@@ -1,10 +1,15 @@
 package com.tistory.dividendcalendar.presentation.setting
 
+import android.content.Intent
+import android.content.pm.PackageInfo
+import android.os.Build
 import android.os.Bundle
 import android.view.View
+import com.tistory.dividendcalendar.BuildConfig
 import com.tistory.dividendcalendar.R
 import com.tistory.dividendcalendar.base.DividendFragment
 import com.tistory.dividendcalendar.databinding.FragmentSettingBinding
+import kotlinx.android.synthetic.main.fragment_setting.*
 
 class SettingFragment : DividendFragment<FragmentSettingBinding>(R.layout.fragment_setting) {
 
@@ -17,5 +22,28 @@ class SettingFragment : DividendFragment<FragmentSettingBinding>(R.layout.fragme
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        val info: PackageInfo =
+            requireActivity().packageManager.getPackageInfo(requireActivity().packageName, 0)
+        val versionName = "ver. ${info.versionName}"
+
+        tvVersion.text = versionName
+        btnQuestion.setOnClickListener {
+            sendEmail()
+        }
+    }
+
+    private fun sendEmail() {
+        val email = Intent(Intent.ACTION_SEND).apply {
+            type = "plain/Text"
+            val address = arrayOf("dlwls5201@gmail.com")
+            putExtra(Intent.EXTRA_EMAIL, address)
+            putExtra(Intent.EXTRA_SUBJECT, "<" + getString(R.string.app_name) + ">")
+            putExtra(
+                Intent.EXTRA_TEXT,
+                "AppVersion :${BuildConfig.VERSION_NAME}\nDevice : ${Build.MODEL}\nAndroid OS : ${Build.VERSION.SDK_INT}\n\n Content :\n"
+            )
+        }
+        startActivity(email)
     }
 }
