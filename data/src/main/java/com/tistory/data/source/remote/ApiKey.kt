a5c8de4db368f8dd3f5dcaa07edb9b68dc915d61
dlwls5201 <dlwls5201@naver.com>
2021-03-08 22:43:25 +0900

    private 변경으로 인한 추가 데이터 업데이트

---
+++ data/src/main/java/com/tistory/data/source/remote/ApiKey.kt
@@ -0,0 +1,8 @@
+package com.tistory.data.source.remote
+
+object ApiKey {
+
+    const val iex_cloud_api_key = "pk_763a1177699243abb9d2f9f099e34a39"
+
+    const val user_habit_api_key = "dev_52feec35ac35e1c4253457f714045451e8f5361c"
+}
